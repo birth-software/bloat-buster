53c0e4a18b27b41cb3cf78b545ee3fc9a0793d8b
David Gonzalez Martin <davidgm94.work@protonmail.com>
2025-01-02 18:13:15 -0600

    Make rendering_initialize work

--- build.c
+++ build.c
@@ -3,6 +3,8 @@
 set -eu
 BUILD_DIR=cache
 mkdir -p $BUILD_DIR
+glslangValidator -V bootstrap/std/shaders/rect.vert -o $BUILD_DIR/rect.vert.spv --quiet
+glslangValidator -V bootstrap/std/shaders/rect.frag -o $BUILD_DIR/rect.frag.spv --quiet
 BUILD_OUT=$BUILD_DIR/build
 TIME_TRACE=1
 if [[ "$TIME_TRACE" == "1" ]]; then
