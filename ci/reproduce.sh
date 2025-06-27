0ada58942285ec9e55efb97e32ac82afdd500a03
David Gonzalez Martin <davidgmbb@gmail.com>
2025-06-27 14:23:18 -0600

    Delete C++ implementation and dead code

--- ci/reproduce.sh
+++ ci/reproduce.sh
@@ -1,8 +1,6 @@
 set -eux
 rm -rf bb-cache self-hosted-bb-cache || true
 ./generate.sh
-./build.sh
-./build/bb test
+CMAKE_PREFIX_PATH=$HOME/dev/llvm/install/llvm_${LLVM_VERSION}_x86_64-linux-${CMAKE_BUILD_TYPE} $HOME/bloat-buster-artifacts/releases/main/compiler_generic_debug reproduce
 mkdir -p $HOME/bloat-buster-artifacts/$(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)/$CMAKE_BUILD_TYPE
 mv ./self-hosted-bb-cache $HOME/bloat-buster-artifacts/$(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)/$CMAKE_BUILD_TYPE/cache
-
