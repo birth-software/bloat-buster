8673f9c8be7be7d00ddf7fed0e2dd12aeaf6f4c0
David Gonzalez Martin <davidgmbb@gmail.com>
2025-06-29 21:18:38 -0600

    First CI reform

--- ci/reproduce.sh
+++ ci/reproduce.sh
@@ -1,6 +1,11 @@
 set -eux
-rm -rf bb-cache self-hosted-bb-cache || true
+
+if [[ -z "${BB_CACHE_DIR:-}" ]]; then
+    BB_CACHE_DIR=self-hosted-bb-cache
+fi
+
+rm -rf bb-cache self-hosted-bb-cache $BB_CACHE_DIR || true
 ./generate.sh
 CMAKE_PREFIX_PATH=$HOME/dev/llvm/install/llvm_${LLVM_VERSION}_x86_64-linux-${CMAKE_BUILD_TYPE} $HOME/bloat-buster-artifacts/releases/main/compiler_generic_debug reproduce
 mkdir -p $HOME/bloat-buster-artifacts/$(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)/$CMAKE_BUILD_TYPE
-mv ./self-hosted-bb-cache $HOME/bloat-buster-artifacts/$(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)/$CMAKE_BUILD_TYPE/cache
+mv $BB_CACHE_DIR $HOME/bloat-buster-artifacts/$(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)/$CMAKE_BUILD_TYPE/cache
