b1c278cfe97e2ead5b6281658e7b9be57771c1eb
David Gonzalez Martin <davidgmbb@gmail.com>
2025-07-01 11:29:06 -0600

    Some more CI reform

--- ci/reproduce.sh
+++ ci/reproduce.sh
@@ -5,7 +5,9 @@ if [[ -z "${BB_CACHE_DIR:-}" ]]; then
 fi
 
 rm -rf bb-cache $BB_CACHE_DIR || true
+
 ./generate.sh
 CMAKE_PREFIX_PATH=$HOME/dev/llvm/install/llvm_${LLVM_VERSION}_x86_64-linux-${CMAKE_BUILD_TYPE} $HOME/bloat-buster-artifacts/releases/main/compiler_generic reproduce
+
 mkdir -p $HOME/bloat-buster-artifacts/$(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)/$CMAKE_BUILD_TYPE
 mv $BB_CACHE_DIR $HOME/bloat-buster-artifacts/$(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)/$CMAKE_BUILD_TYPE/cache
