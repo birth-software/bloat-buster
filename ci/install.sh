4c9fc62759e57bd2855def6f440482ee20b6c573
David Gonzalez Martin <davidgmbb@gmail.com>
2025-07-01 10:36:39 -0600

    Use LLVM with assertions for debug build

--- ci/install.sh
+++ ci/install.sh
@@ -12,7 +12,7 @@ if [[ -z "${BB_BUILD_NATIVE:-}" ]]; then
 fi
 
 if [[ "$BB_BUILD_DEBUG" == "1" ]]; then
-    cp $HOME/bloat-buster-artifacts/$(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)/Release/cache/debug_none_di_generic/compiler $HOME/bloat-buster-artifacts/releases/$(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)/compiler_generic_debug
+    cp $HOME/bloat-buster-artifacts/$(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)/Release-assertions/cache/debug_none_di_generic/compiler $HOME/bloat-buster-artifacts/releases/$(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)/compiler_generic_debug
 fi
 
 if [[ "$BB_BUILD_NATIVE" == "1" ]]; then
