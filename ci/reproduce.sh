52de906be0f7be1129bfb668b5a3e624c3b2cb72
David Gonzalez Martin <davidgmbb@gmail.com>
2025-07-18 21:18:41 -0600

    Fix paths

--- ci/reproduce.sh
+++ ci/reproduce.sh
@@ -7,6 +7,6 @@ fi
 rm -rf bb-cache $BB_CACHE_DIR || true
 ./generate.sh
 CMAKE_PREFIX_PATH=$HOME/dev/llvm/install/llvm_${LLVM_VERSION}_x86_64-linux-${CMAKE_BUILD_TYPE} $HOME/bloat-buster-artifacts/releases/main/compiler_generic compile src/compiler.bbb
-bb-cache/debug_none_di_native/compiler reproduce
+bb-cache/x86_64_linux_development_di_native/compiler reproduce
 mkdir -p $HOME/bloat-buster-artifacts/$(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)/$CMAKE_BUILD_TYPE
 mv $BB_CACHE_DIR $HOME/bloat-buster-artifacts/$(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)/$CMAKE_BUILD_TYPE/cache
