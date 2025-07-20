f1f3753f6c3d80d1b6f6d9247a1fe34d8f017087
David Gonzalez Martin <davidgmbb@gmail.com>
2025-07-20 07:54:29 -0600

    Fix CPU features crash

--- ci/reproduce.sh
+++ ci/reproduce.sh
@@ -6,7 +6,7 @@ fi
 
 rm -rf bb-cache $BB_CACHE_DIR || true
 ./generate.sh
-CMAKE_PREFIX_PATH=$HOME/dev/llvm/install/llvm_${LLVM_VERSION}_x86_64-linux-${CMAKE_BUILD_TYPE} $HOME/bloat-buster-artifacts/releases/main/compiler_generic compile src/compiler.bbb debug true true
+CMAKE_PREFIX_PATH=$HOME/dev/llvm/install/llvm_${LLVM_VERSION}_x86_64-linux-${CMAKE_BUILD_TYPE} $HOME/bloat-buster-artifacts/releases/main/compiler_generic_debug compile src/compiler.bbb debug true true
 bb-cache/x86_64_linux_debug_di_native/compiler reproduce
 mkdir -p $HOME/bloat-buster-artifacts/$(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)/$CMAKE_BUILD_TYPE
 mv $BB_CACHE_DIR $HOME/bloat-buster-artifacts/$(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)/$CMAKE_BUILD_TYPE/cache
